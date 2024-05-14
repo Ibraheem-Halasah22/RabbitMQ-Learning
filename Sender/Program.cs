cd8c49162292a762381fe9f00a5f4613591a46d6
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2024-05-14 11:07:49 +0300

    Sender code implemented and tested successfully

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+DotEnv.DotEnv.LoadDotEnvPathFromTheSolutionDirectory();
+var factory = new ConnectionFactory
+{
+    Uri = new Uri(Environment.GetEnvironmentVariable("MQ_URL"))
+};
+
+string queueName = Environment.GetEnvironmentVariable("Q_NAME");
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: queueName,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var message = new
+{
+    Name = "First",
+    Message= "My Message"
+};
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+await channel.BasicPublishAsync(exchange: "", routingKey: queueName, body: body);
